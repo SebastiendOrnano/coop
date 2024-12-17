91c4c5a8b2133a18954330260234b6b945e7d5e8
Sébastien d'Ornano <sebastien@sebastien-Latitude-E5440>
2024-12-17 12:47:30 +0100

    changement cosmetique

---
+++ b_about/orga_CGV.sql
@@ -0,0 +1,24 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell.sql') AS properties;
+
+SET orga_dept_name = (SELECT orga_dept_name FROM orga_dept WHERE orga_dept_id = '1');
+
+
+select 
+    'title'   as component,
+    'CGV' as contents,
+    TRUE      as center;
+
+
+SELECT
+'text' as component;
+SELECT
+'Les présentes Conditions Générales de ventes *ci-après CGV* régissent les règles commerciales des prestations réalisées par '||$orga_dept_name||'. L''utilisateur reconnaît avoir pris connaissance des CGU et s''engage à les respecter.' as contents_md;
+
+select 
+     'foldable' as component;
+        ;
+select 
+   section_title        as title,
+   'foldablemargin'     as class,
+   section_content      as description_md
+from info_sections where section_category="CGV" AND section_publication_status='yes' ORDER BY section_number ASC;
