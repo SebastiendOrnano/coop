4ad2cd01b86c10b20aec27daa7492c14fbd3c497
SdO <sdornano@gmail.com>
2025-03-12 14:00:56 +0100

    Add files via upload

---
+++ a_panels/panel_editor_3.sql
@@ -0,0 +1,90 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_3.sql') AS properties;
+
+SET username = (
+    SELECT users.username FROM users
+    INNER JOIN sessions ON users.user_id = sessions.user_id
+    WHERE sessions.session_token = sqlpage.cookie('session_token')
+); 
+
+
+select 
+    'datagrid'                                      as component,
+    'Panneau d''administration éditeur projets'         as title,
+    'project_supervisor_panel'                               as id,
+    'datagrid_panel'                                as class,
+    'Vous êtes loggé comme :  '||$username      as description_md;
+
+select 
+    'GROUPES DE TRAVAIL'                          as title,
+    '/c_workspaces/workspace_main_display_3.sql'       as link,
+    'Groupes de travail'                          as description,
+    'users-group'                                 as icon,
+    'blue'                                        as color;
+
+select 
+    'PERSONS'                            as title,
+    '/d_prm1/prm1_main_display_3.sql'           as link,
+    'Création, suivi des personnes physiques' as description,
+    'user-plus'                                      as icon,
+     'green'                                         as color;
+
+select 
+    'COMPAGNIES'                     as title,
+    '/d_prm2/prm2_main_display_3.sql'               as link,
+    'Création, suivi des organisations'             as description,
+    'affiliate'                                     as icon,
+    'red'                                           as color;
+
+select 
+    'GROUPS'                     as title,
+    '/d_prm3/prm3_main_display_3.sql'               as link,
+    'Création, suivi des groupes'                   as description,
+    'users-group'                                   as icon,
+    'blue'                                          as color;
+
+select 
+    'PROJECTS'                                   as title,
+    '/e_projects/project_main_display_3.sql'       as link,
+    'Gestion des projets'         as description,
+    'chart-dots-2'                                         as icon,
+    'yellow'                                        as color;
+
+
+select 
+    'datagrid'                                  as component,
+    'Activités au sein de la communauté'         as title,
+    'supervisor_editor'                         as id,
+    'datagrid_panel'                            as class,
+    'Vous êtes loggé comme :  '||$username      as description_md;
+
+
+select 
+    'Gestion des événenements'                    as title,
+    '/c_meetings/meeting_main_display_3.sql'      as link,
+    'Gestion des évenements'                      as description,
+    'list-details'                                as icon,
+    'green'                                       as color;
+
+select 
+    'Actus'                                      as title,
+    '/b_news/news_main_display_3.sql'            as link,
+    'News'                                       as description,
+    'text-scan-2'                                as icon,
+    'orange'                                      as color;
+
+select 
+    '/a_rss/rss_main_display_3.sql'     as link,
+    'red'                               as color,
+    'RSS'                               as title,
+    'Liens RSS à copier'                as description,
+    'rss'                               as icon;
+
+select 
+    '/a_process/process_editor_display_3.sql'  as link,
+    'lue'                                      as color,
+    'PROCESS'                                  as title,
+    'Processus'                                as description,
+    'automation'                               as icon;
+
+
+
---
+++ a_panels/panel_supervisor_4.sql
@@ -0,0 +1,149 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
+
+SET username = (
+    SELECT users.username FROM users
+    INNER JOIN sessions ON users.user_id = sessions.user_id
+    WHERE sessions.session_token = sqlpage.cookie('session_token')
+); 
+
+
+
+select 
+    'datagrid'                                      as component,
+    'GESTION DES PROJETS'                           as title,
+    'project_supervisor_panel'                      as id,
+    'datagrid_panel'                                as class,
+    'Vous êtes loggé comme :  '||$username          as description_md;
+
+select 
+    'PROJECTS'                                   as title,
+    '/e_projects/project_main_display_4.sql'       as link,
+    'Gestion des projets'         as description,
+    'chart-dots-2'                                         as icon,
+    'yellow'                                        as color;
+
+
+select 
+    'PERSONS'                            as title,
+    '/d_prm1/prm1_main_display_4.sql'           as link,
+    'Création, suivi des personnes physiques' as description,
+    'user-plus'                                      as icon,
+     'green'                                         as color;
+
+select 
+    'COMPAGNIES'                     as title,
+    '/d_prm2/prm2_main_display_4.sql'               as link,
+    'Création, suivi des organisations'             as description,
+    'affiliate'                                     as icon,
+    'red'                                           as color;
+
+select 
+    'TEAMS'                     as title,
+    '/d_prm3/prm3_main_display_4.sql'               as link,
+    'Création, suivi des groupes'                   as description,
+    'users-group'                                   as icon,
+    'blue'                                          as color;
+
+select           
+    'WORKSPACES'                                 as title,
+    '/c_workspaces/workspace_main_display_4.sql'  as link,
+    'Groupes de travail'                          as description,
+    'users-group'                                 as icon,
+    'green'                                        as color;
+
+
+
+
+select 
+    'datagrid'                                       as component,
+    'GESTION DU SITE INTERNET'                       as title,
+    'supervisor_panel'                               as id,
+    'datagrid_panel'                                 as class,
+    'Vous êtes loggé comme :  '||$username           as description_md;
+
+select 
+    'Infos site'                                    as title,
+    'Infos administratives... sur le site'          as description,
+    'help-octagon'                                  as icon,
+    'green'                                         as color,
+    '/a_info/section_main_display_4.sql'            as link;
+
+select 
+    'Gestion de la faq'                             as title,
+    'Questions - Réponses'                          as description,
+    'help-octagon'                                  as icon,
+    'blue'                                          as color,
+    '/b_faq/faq_main_display_4.sql'                 as link;
+
+select 
+    'Gestion des membres'                            as title,
+    '/b_members/member_main_display_4.sql'           as link,
+    'Création, suivi des membres de l''organisation' as description,
+    'user-plus'                                      as icon,
+     'green'                                         as color;
+
+select 
+    'Gestion des organisations'                     as title,
+    '/b_orga/orga_main_display_4.sql'               as link,
+    'Création, suivi des organisations'             as description,
+    'affiliate'                                     as icon,
+    'red'                                           as color;
+
+select 
+    'Gestion doc'                                   as title,
+    '/b_orga_doc/orga_doc_main_display_4.sql'       as link,
+    'Gestion des documents institutionnels'         as description,
+    'books'                                         as icon,
+    'yellow'                                        as color;
+
+select 
+    'Gestion des partenaires'                         as title,
+    '/b_orga_partners/orga_partner_display_4.sql'     as link,
+    'Gestion des partenaires institutionnels'         as description,
+    'affiliate'                                    as icon,
+    'orange'                                          as color;
+
+select 
+    'Gestion des lieux'                             as title,
+    '/b_orga_places/orga_place_main_display_4.sql'             as link,
+    'Gestion des lieux'                             as description,
+    'map-pin'                                       as icon,
+    'red'                                           as color;
+
+select 
+    'EVENTS'                                        as title,
+    '/b_events/event_main_display_4.sql'            as link,
+    'Gestion des evenements publics'                as description,
+    'confetti'                                      as icon,
+    'blue'                                          as color;
+
+select 
+    'MEETINGS'                                      as title,
+    '/c_meetings/meeting_main_display_4.sql'        as link,
+    'Gestion des evenements privés'                 as description,
+    'calendar-clock'                                as icon,
+    'orange'                                        as color;
+
+
+select 
+    'NEWS'                                     as title,
+    '/b_news/news_main_display_4.sql'          as link,
+    'Gestion des actualités'                   as description,
+    'text-scan-2'                              as icon,
+    'green'                                    as color;
+
+
+
+select 
+    '/a_rss/rss_main_display_4.sql'     as link,
+    'red'                              as color,
+    'RSS'                               as title,
+    'Liste des liens RSS à copier'      as description,
+    'rss'                               as icon;
+
+select 
+    '/a_process/process_supervisor_display_4.sql'  as link,
+    'lue'                                      as color,
+    'PROCESS'                                  as title,
+    'Processus'                                as description,
+    'automation'                               as icon;
