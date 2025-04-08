95cdac7583edc0f264927eedcc58854238629e74
SdO <sdornano@gmail.com>
2025-04-08 13:31:44 +0200

    Delete e_project_sprints directory

--- e_project_sprints/sprint_main_create_alert1.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
-
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'La date de fin est antérieure à la date de début
-    [Reprenez votre saisie](/e_project_sprints/sprint_main_form_4.sql?project_id='||$project_id||')' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
--- e_project_sprints/sprint_main_create_alert2.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
-
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'La date de fin réelle est antérieure à la date de début réelle
-    [Reprenez votre saisie](/e_project_sprints/sprint_main_edit_4.sql?sprint_id='||$sprint_id||')' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
