07bc3ff98ce7fd374dea96f455984deb26e3dda0
SdO <sdornano@gmail.com>
2025-04-08 13:39:44 +0200

    Add files via upload

---
+++ e_project_building_rooms_pictures/project_picture_gallery_3.sql
@@ -0,0 +1,38 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_3.sql') AS properties;
+
+SET building_id          = select building_id  FROM project_building WHERE project_id = $project_id;
+SET room_id              = select room_id from project_building_rooms where building_id=$building_id;
+SET room_picture_id      = select room_picture_id from project_building_rooms_pictures where room_id=$room_id;
+SET project_name         =   (select project_name  FROM project WHERE project_id = $project_id);
+
+
+select 
+    'breadcrumb' as component;
+select 
+    'Home' as title,
+    '/'    as link;
+select 
+    'Gestion éditeur'         as title,
+    '/a_panels/panel_supervisor_3.sql' as link;
+ select 
+    'Retour au hub du projet'            as title,
+    '/e_project/project_hub_display_3.sql?project_id='||$project_id     as link;   
+ 
+
+select 
+    'datagrid'              as component,
+    'Galerie des photos du projet  :'||$project_name        as title;
+select 
+    p.room_picture_title                     as title,
+    p.room_picture_url                       as link,
+    p.room_picture_url                       as image_url,
+    b.building_name ||'---'|| r.room_name    as footer
+    FROM project_building_rooms_pictures     as p
+    LEFT JOIN (select room_id, room_name, building_id from project_building_rooms) as r
+    on p.room_id = r.room_id
+    LEFT JOIN (select building_id, building_name, project_id from project_building) as b
+    on r.building_id = b.building_id
+    WHERE b.project_id = $project_id
+    ORDER BY p.room_picture_id ASC;
+
+
---
+++ e_project_building_rooms_pictures/room_picture_form_step2_3.sql
@@ -0,0 +1,53 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_3.sql') AS properties;
+
+select 
+    'breadcrumb' as component;
+select 
+    'Home' as title,
+    '/'    as link;
+select 
+    'Gestion éditeur'         as title,
+    '/a_panels/panel_editor_3.sql' as link;
+ select 
+    'hub du projet'            as title,
+    '/e_project/project_hub_display_3.sql?project_id='||$project_id     as link;  
+select 
+    '/e_project_building_rooms/room_main_display_3.sql?building_id='||$building_id    as link,
+    'liste des lieux'  as title;
+select 
+    '/e_project_building_rooms_pictures/project_picture_display_3.sql?project_id='||$project_id    as link,
+    'gestion des photos du projet'  as title;
+    
+select 
+    'form'   as component,
+    '' as validate;
+SELECT
+    'building_id' as name,
+    TRUE as readonly,
+    'Id ' as label,
+    1 as width,
+     $building_id as value;
+SELECT
+    'building_name' as name,
+    TRUE as readonly,
+    'Nom du Bâtiment' as label,
+    5 as width,
+    (select building_name  FROM project_building WHERE building_id = $building_id) as value;
+
+
+select 
+    'form'   as component,
+    'choisir la pièce' as validate,
+    'clear' as reset,
+    '/e_project_building_rooms_pictures/room_picture_create_step2_0.sql?building_id='||$building_id  as action;
+select 
+    'room_id' as name,
+    'choisir la pièce dans le bâtiment' as label,
+    'select' as type,
+    12 AS width,
+    TRUE as searchable,
+   'Saisissez quelques lettres du titre que vous cherchez ou choississez dans la liste' as empty_option,
+    json_group_array(json_object('label',room_name,'value', room_id))  AS options
+    FROM project_building_rooms
+    WHERE building_id=$building_id
+    ORDER BY  room_name ASC;
