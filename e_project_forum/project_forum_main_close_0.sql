8d34f174bef6b5190f66585b7076e888af48c70d
SdO <sdornano@gmail.com>
2025-04-08 13:35:11 +0200

    Delete e_project_forum directory

--- e_project_forum/project_forum_main_close_0.sql
+++
@@ -1,9 +0,0 @@
-UPDATE project_forum_topics
-SET
-project_forum_topic_status='closed',
-modified_at=CURRENT_TIMESTAMP
-WHERE project_forum_topic_id=$project_forum_topic_id
-
-RETURNING 
-'redirect' AS component, 
-'/e_project_forum/project_forum_main_display_4.sql' AS link;
