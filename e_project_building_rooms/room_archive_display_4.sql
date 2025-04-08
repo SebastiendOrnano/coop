731bdab0373ae6d4709fb7ee299b9c3be798fcde
SdO <sdornano@gmail.com>
2025-04-08 13:39:09 +0200

    Add files via upload

---
+++ e_project_building_rooms/room_archive_display_4.sql
@@ -0,0 +1,90 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
+
+SET project_id = select project_id from project_building where building_id=$building_id;
+
+
+select 
+    'breadcrumb' as component;
+select 
+    'Home' as title,
+    '/'    as link;
+select 
+    'Gestion Superviseur'         as title,
+    '/a_panels/panel_supervisor_4.sql' as link;
+ select 
+    'Retour à la liste des projets'            as title,
+    '/e_project/project_main_display_4.sql'     as link;   
+ select 
+    'Retour au hub du projet'            as title,
+    '/e_project/project_hub_display_4.sql?project_id='||$project_id     as link;   
+ select 
+    'Retour aux bâtiments du projet'            as title,
+    '/e_project_building/building_main_display_4.sql?project_id='||$project_id     as link;   
+
+
+select 
+    'datagrid'              as component,
+    'panel_building_display' as id,
+    'Gestion des bâtiments' as title;
+
+select 
+    '/e_project_building_rooms/room_main_form_4.sql?building_id='||$building_id       as link,
+    'Création d''une nouvelle salle'                  as description,
+    'building-community'                                      as icon,
+    'yellow'                                         as color;
+
+select 
+     '/e_project_building_rooms/room_archive_display_4.sql?building_id='||$building_id             as link,
+    'Visualiser les salles achivées'           as description,
+    'archive'                                     as icon,
+    'green'                                              as color;
+
+select 
+    'divider' as component,
+    'Nom du bâtiment de rattachement'    as contents,
+    'blue' as color;
+
+select 
+    'form'   as component,
+    '' as validate;
+
+SELECT
+    'building_id' as name,
+    TRUE as readonly,
+    'Id ' as label,
+    1 as width,
+   $building_id as value;
+
+SELECT
+    'building_name' as name,
+    TRUE as readonly,
+    'Nom du lieu de rattachement' as label,
+    11 as width,
+    (select building_name  FROM project_building WHERE building_id = $building_id) as value;
+ 
+select 
+    'divider' as component,
+    'Liste des pièces ou des salles rattachées au lieu'    as contents,
+    'blue' as color;
+
+SELECT 
+    'table'               AS component, 
+    'Liste des pièces ou des salles rattachées au lieu' AS title, 
+    TRUE                  AS sort, 
+    'View'                AS markdown, 
+    FALSE                 AS search;
+
+SELECT
+    room_id            AS Id,
+    room_name            AS Nom,
+    room_type             as Type,
+'[Edit](/e_project_building_rooms/room_main_edit_4.sql?room_id=' 
+||room_id|| 
+') &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [Delete](/e_project_building_rooms/room_main_delete_alert_4.sql?room_id=' 
+||room_id||
+')'    AS View
+FROM project_building_rooms
+WHERE building_id = $building_id and room_status ='archived'
+ORDER BY room_id ASC;
+
+
---
+++ e_project_building_rooms/room_main_form_4.sql
@@ -0,0 +1,119 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
+
+select 
+    'breadcrumb' as component;
+select 
+    'Home' as title,
+    '/'    as link;
+select 
+    'Gestion Superviseur'         as title,
+    '/a_panels/panel_supervisor_4.sql' as link;
+ select 
+    'liste des projets'            as title,
+    '/e_project/project_main_display_4.sql'     as link;   
+ select 
+    'hub du projet'            as title,
+    '/e_project/project_hub_display_4.sql?project_id='||$project_id     as link;   
+ select 
+    'bâtiments du projet'            as title,
+    '/e_project_building/building_main_display_4.sql?project_id='||$project_id     as link;    
+select 
+    'lieu de rattachement'            as title,
+    '/e_project_building_rooms/room_main_display_3.sql?building_id='||$building_id     as link;
+
+SELECT 
+    'form' as component,
+    'Créer une salle' as title,
+    'Créer' as validate,
+    'Clear' as reset,
+    '/e_project_building_rooms/room_main_create_0.sql?building_id='||$building_id as action;
+
+SELECT
+    'room_name' as name,
+    'text' as type,  
+    'Nom' as label,
+    6 AS width,
+    TRUE as required;
+
+
+ SELECT 
+    'room_type'            as name,
+    'Nature de la salle'                 as label,
+    'select'                       as type,
+    3                              AS width,
+    'Choisir la nature de la salle dans la liste...'    as  empty_option,
+    json_group_array(json_object('label',i.choice_label, 'value', i.choice_value))  AS options
+    FROM choices_items AS i
+    LEFT JOIN (select choice_category_id, choice_category_name from choices_categories)  AS c 
+    ON i.choice_category_id = c.choice_category_id
+    where choice_category_name="room_type"
+    ORDER BY  i.choice_label ASC;
+
+SELECT
+    'room_summary' as name,
+    'textarea' as type,
+    'Disposition, équipement, lumière...' as description,
+    'Présentation de la salle' as label;
+
+SELECT
+    'room_todo' as name,
+    'textarea' as type,
+    'Travaux à faire...' as description,
+    'Travaux à faire' as label;
+
+ SELECT 
+    'room_accessibility'            as name,
+    'Accessibilité'                 as label,
+    'select'                       as type,
+    3                              AS width,
+    'Choisir un statut dans la liste...'    as  empty_option,
+    json_group_array(json_object('label',i.choice_label, 'value', i.choice_value))  AS options
+    FROM choices_items AS i
+    LEFT JOIN (select choice_category_id, choice_category_name from choices_categories)  AS c 
+    ON i.choice_category_id = c.choice_category_id
+    where choice_category_name="building_accessibility"
+    ORDER BY  i.choice_label ASC;
+
+SELECT
+    'room_capacity_seats' as name,
+    'number' as type,  
+    'Nombre max' as label,
+    'assis' as suffix,
+    3 as width;
+
+SELECT
+    'room_capacity_standup' as name,
+    'number' as type,  
+    'Nombre max' as label,
+    'debout' as suffix,
+    3 as width;
+
+SELECT
+    'room_width' as name,
+    'number' as type,  
+    'largeur en mm' as label,
+    4 as width;
+
+SELECT
+    'room_length' as name,
+    'number' as type,  
+    'longueur en mm' as label,
+    4 as width;
+
+SELECT
+    'room_height' as name,
+    'number' as type,  
+    'largeur' as label,
+    4 as width;
+
+SELECT
+    'room_wall_material' as name,
+    'text' as type,  
+    'nature des murs' as label,
+    6 as width;
+
+SELECT
+    'room_wall_material' as name,
+    'text' as type,  
+    'nature du sol' as label,
+    6 as width;
